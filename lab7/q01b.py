a8bf8b71a4cf32c091ef33ae6a6b3e839f4373c7
gabi 3 <gabizinhaloures73@gmail.com>
2026-05-20 15:11:42 -0300

    Create q01b.py

---
+++ lab7/q01b.py
@@ -0,0 +1,8 @@
+
+soma = 0
+cont = 1 #adicionar variavel cont 
+while cont <= 10: 
+    num = int(input("Digite um número para somar: "))
+    soma += num
+    cont +=1 #adicionar o contador a cada vez que a pessoa digitar um numero
+print(soma)
