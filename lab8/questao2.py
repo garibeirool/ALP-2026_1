d64572b7f21462fe3ce59d827bd0607bfe2767ed
gabi 3 <gabizinhaloures73@gmail.com>
2026-05-27 15:25:13 -0300

    Create questao2.py

---
+++ lab8/questao2.py
@@ -0,0 +1,10 @@
+cont = 5
+while cont > 0: 
+    num = int(input("Digite um número inteiro: "))
+    cont -= 1
+    if num % 2 == 0: #se você digita um numero par, o codigo pula a proxima linha
+        continue
+    print(f'{num} é um número ímpar') #quando você digita um numero impar, o código para
+
+
+
