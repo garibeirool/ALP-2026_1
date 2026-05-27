b078eca4394d4686d007cf1f2c7a235bbeab24ca
gabi 3 <gabizinhaloures73@gmail.com>
2026-05-27 15:25:05 -0300

    Create questao5.py

---
+++ lab8/questao5.py
@@ -0,0 +1,17 @@
+soma = 0
+print(0 fechar conta 1donuts = R$ 6,00 2cookies = R$ 5,00 3balas = R$ 2,00  4sanduiches = R$ 3,50)
+preco_final = 0
+int(input("qual numero você deseja?:"))
+if opcao==1:
+    total +=6
+elif opcao==2:
+    total+=5
+elif opcao == 3:
+    total+=4
+elif opcao == 4:
+    total +=3
+elif opcao==0:
+    print(total)
+    break
+else:
+    continue
